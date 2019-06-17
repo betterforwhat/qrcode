e033257e66a309b7f9a77368d9fead8561d52d36
maroco <betterforwhat@gmail.com>
2019-06-17 19:58:20 +0900

    setting

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Header.css';
+
+class Header extends Component {
+	render() {
+		return (
+		<header>
+			HI
+		</header>
+		);
+	}
+}
+
+export default Header;
